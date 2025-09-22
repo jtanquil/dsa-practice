7a55c62ccd82933fe4511c99a4c21fb9289fffce
jtanquil <jose.tanquiilut@gmail.com>
2025-09-22 18:52:32 +0800

    moved insert to node

--- python/ds-classes/binary-tree.py
+++ python/ds-classes/binary-tree.py
@@ -14,6 +14,15 @@ class TreeNode():
   def __str__(self):
     return f'{self.val}'
 
+  def skew(self):
+    left_height = 0 if self.left is None else self.left.height
+    right_height = 0 if self.right is None else self.right.height
+    
+    return right_height - left_height
+
+  def is_imbalanced(self):
+    return self.skew() < -1 or self.skew() > 1
+
 class BSTNode(TreeNode):
   def __init__(self, val):
     super().__init__(val)
@@ -36,6 +45,34 @@ class BSTNode(TreeNode):
   def __neq__(self, node):
     return self.val != node.val
 
+  def insert(self, val):
+    if self.val < val:
+      if self.right is None:
+        self.right = BSTNode(val)
+        self.right.parent = self
+        self.height = 1 if self.left is None else max(1, 1 + self.left.height)
+      else:
+        self.right.insert(val)
+    else:
+      if self.left is None:
+        self.left = BSTNode(val)
+        self.left.parent = self
+        self.height = 1 if self.right is None else max(1, 1 + self.right.height)
+      else:
+        self.left.insert(val)
+
+    if self.is_imbalanced():
+      self.rebalance()
+
+  def rebalance(self):
+    pass
+
+  def left_rotate(self):
+    pass
+
+  def right_rotate(self):
+    pass
+
 class BinarySearchTree(Set):
   def __init__(self):
     self.root = None
@@ -45,27 +82,19 @@ class BinarySearchTree(Set):
     return self.node_count
 
   def insert(self, val):
-    def insert_r(val, node):
-      if node.val < val:
-        if node.right is None:
-          node.right = BSTNode(val)
-          node.right.parent = node
-        else:
-          insert_r(val, node.right)
-      else:
-        if node.left is None:
-          node.left = BSTNode(val)
-          node.left.parent = node
-        else:
-          insert_r(val, node.left)
-
     if self.root is None:
       self.root = BSTNode(val)
     else:
-      insert_r(val, self.root)
+      self.root.insert(val)
+
+    self.node_count += 1
 
   def build(self, iter):
-    pass
+    self.root = None
+    self.node_count = 0
+
+    for ele in iter:
+      self.insert(ele)
   
   def __getitem__(self, key):
     pass
@@ -120,6 +149,7 @@ class BinarySearchTree(Set):
     return [] if self.root is None else dfs_r(self.root, callback, [], order)
 
 if __name__ == "__main__":
+  orders = ["pre-order", "in-order", "post-order"]
   seed(3413)
   test = [randint(0, 20) for i in range(10)]
   tree = BinarySearchTree()
@@ -130,5 +160,14 @@ if __name__ == "__main__":
   print(f'list: {test}')
   print(f'tree (bfs): {tree}')
 
-  for order in ["pre-order", "in-order", "post-order"]:
+  for order in orders:
+    print(f'{order} dfs: {tree.dfs(lambda node: str(node), order)}')
+
+  build_test = [randint(0, 20) for i in range(10)]
+  tree.build(build_test)
+
+  print(f'list: {build_test}')
+  print(f'tree (bfs): {tree}')
+
+  for order in orders:
     print(f'{order} dfs: {tree.dfs(lambda node: str(node), order)}')
