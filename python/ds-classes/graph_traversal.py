12f029f7880dc0628bce31a90f9633c2e39419e8
jtanquil <jose.tanquiilut@gmail.com>
2025-10-15 17:51:49 +0800

    finished dfs, connected components

--- python/ds-classes/graph_traversal.py
+++ python/ds-classes/graph_traversal.py
@@ -10,14 +10,20 @@ def dfs(graph, incoming_vertex):
     for outgoing_vertex in incident_vertices:
       if outgoing_vertex not in visited_vertices:
         visited_vertices[outgoing_vertex] = graph.get_edge(incoming_vertex, outgoing_vertex)
-        return _dfs(graph, outgoing_vertex, visited_vertices)
+        _dfs(graph, outgoing_vertex, visited_vertices)
     
     return visited_vertices
 
-  return _dfs(graph, incoming_vertex, {incoming_vertex: []})
+  return _dfs(graph, incoming_vertex, {incoming_vertex: None})
 
-def get_weights(graph):
-  pass
+def connected_components(graph):
+  components = []
+
+  for vertex in graph.vertices():
+    if len(list(filter(lambda x: vertex in x, components))) == 0:
+      components.append(dfs(graph, vertex))
+
+  return components
 
 if __name__ == "__main__":
   graph = GraphAdjacencyMap(is_directed = True)
@@ -38,4 +44,19 @@ if __name__ == "__main__":
   
   for i in range(5):
     visited = dfs(graph, i)
+    print(f'dfs on {i}:')
     print({i: f'{visited[i]}' for i in visited})
+
+  two_components = GraphAdjacencyMap()
+
+  for i in range(6):
+    two_components.insert_vertex(i)
+
+  two_components.insert_edge(0, 1, 1)
+  two_components.insert_edge(1, 2, 1)
+  two_components.insert_edge(2, 0, 1)
+  two_components.insert_edge(3, 4, 1)
+  two_components.insert_edge(4, 5, 1)
+  two_components.insert_edge(5, 3, 1)
+
+  print(connected_components(two_components))
